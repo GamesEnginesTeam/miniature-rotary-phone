89056510f808cb354bb3c7633c2cb9773664c057
DanielsDubstep35 <Danielmendes2@icloud.com>
2024-05-16 11:18:14 +0100

    Gizmo Button Added and fixed bugs
    Added button to toggle Gizmos. Also fixed some errors.
    
    Co-Authored-By: Lazyice1172 <78085794+lazyice1172@users.noreply.github.com>
    Co-Authored-By: Keith <98464156+kylreia@users.noreply.github.com>

--- GodotBoids/Boid Settings.gd
+++ GodotBoids/Boid Settings.gd
@@ -17,6 +17,9 @@ var Banking: HSlider = $Banking/HSlider2
 @onready
 var Damping: HSlider = $Damping/HSlider2
 
+@onready
+var GizmosButton: Button = $Gizmos/Button
+
 # Labels
 @onready
 var Speed_Label: Label = $Speed/Label2
@@ -45,10 +48,24 @@ func change_values():
 		boid.max_speed = Max_Speed.value
 		boid.max_force = Max_Force.value
 		boid.damping = Damping.value
-		boid.banking = Banking.value    
+		boid.banking = Banking.value  
+		if GizmosButton.pressed:
+			toggleGizmos()
 		
 	Speed_Label.text = str(Speed.value)
 	Max_Speed_Label.text = str(Max_Speed.value)
 	Max_Force_Label.text = str(Max_Force.value)
 	Damping_Label.text = str(Damping.value)
 	Banking_Label.text = str(Banking.value)
+		
+func toggleGizmos():
+	var all_boids = get_tree().get_nodes_in_group("Boid")
+	
+	for boid in all_boids:
+		if GizmosButton.pressed && GizmosButton.button_pressed:
+			GizmosButton.text = "Enabled"
+			boid.draw_gizmos_recursive(true)
+		elif GizmosButton.pressed && !GizmosButton.button_pressed:
+			GizmosButton.text = "Disabled"
+			boid.draw_gizmos_recursive(false)
+	
