6662b5872c5f48cfca23657efdeb93c2a953a5a6
DanielsDubstep35 <Danielmendes2@icloud.com>
2024-05-16 04:20:23 +0100

    Created EbbAndFlow Behaviour 🌊
    Created a new Behaviour that moves the boid from one place to another, and back to the original place.
    
    Co-Authored-By: Lazyice1172 <78085794+lazyice1172@users.noreply.github.com>
    Co-Authored-By: Keith <98464156+kylreia@users.noreply.github.com>

---
+++ GodotBoids/world/Behaviours/EbbAndFlow.gd
@@ -0,0 +1,38 @@
+class_name EbbAndFlow extends SteeringBehavior
+
+# This Godot Boid behaviour is original, and involves the boid Going towards the center, and then some more distance away from the center.
+# It also switches directions when it reaches the max or min distance from the center.
+var min_radius: float = -50.0
+var max_radius: float = 50.0
+
+func _ready():
+	boid = get_parent()
+
+func _process(delta):
+	pass
+
+# function returns force vector to apply to boid
+func calculate() -> Vector3:
+	# get the boid's position
+	var position = boid.global_transform.origin
+
+	# get the center of the map
+	var center = Vector3(0, boid.global_transform.origin.y, 0)
+
+	# get the vector from the boid to the center
+	var to_center = center - position
+
+	# get the distance from the boid to the center
+	var distance = to_center.length()
+
+	var force = Vector3(0, 0, 0)
+
+	# if the boid is too close to the center, move it away
+	if distance < min_radius:
+		force = to_center.normalized() * boid.max_speed
+	elif distance > max_radius:
+		force = to_center.normalized() * -boid.max_speed
+	else:
+		force = to_center.normalized() * boid.max_speed
+
+	return force
