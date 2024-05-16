11197b84b31bedccdc25460823d414a74afe1f89
DanielsDubstep35 <Danielmendes2@icloud.com>
2024-05-16 06:49:27 +0100

    Particles for LightBug 🔥⚡️
    Particles were created for the fireflies. They now leave red and yellow streaks in the sky.
    
    Co-Authored-By: Lazyice1172 <78085794+lazyice1172@users.noreply.github.com>
    Co-Authored-By: Keith <98464156+kylreia@users.noreply.github.com>

--- GodotBoids/world/Behaviours/EbbAndFlow.gd
+++ GodotBoids/world/Behaviours/EbbAndFlow.gd
@@ -2,8 +2,8 @@ class_name EbbAndFlow extends SteeringBehavior
 
 # This Godot Boid behaviour is original, and involves the boid Going towards the center, and then some more distance away from the center.
 # It also switches directions when it reaches the max or min distance from the center.
-var min_radius: float = -50.0
-var max_radius: float = 50.0
+var min_radius: float = 5.0
+var max_radius: float = 10.0
 
 func _ready():
 	boid = get_parent()
@@ -11,26 +11,13 @@ func _ready():
 func _process(delta):
 	pass
 
-# function returns force vector to apply to boid
 func calculate() -> Vector3:
-	# get the boid's position
-	var position = boid.global_transform.origin
-
-	# get the center of the map
 	var center = Vector3(0, boid.global_transform.origin.y, 0)
-
-	# get the vector from the boid to the center
-	var distance = boid.global_transform.origin.distance_to(center)
-
-	var force = Vector3(0, 0, 0)
-
-	# if the boid is too close to the center, move it away
+	var direction = center - boid.global_transform.origin
+	var distance = direction.length()
 	if distance < min_radius:
-		force = (position - center).normalized() * boid.max_speed
-	# if the boid is too far from the center, move it closer
+		return direction.normalized() * boid.max_speed
 	elif distance > max_radius:
-		force = (center - position).normalized() * boid.max_speed
-		
-	print(force)
-
-	return force
+		return direction.normalized() * boid.max_speed
+	else:
+		return direction.normalized() * boid.max_speed
